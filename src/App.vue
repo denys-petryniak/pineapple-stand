3292985f14eaf6d5e7a63d03d4579202c627bb74
Denys Petryniak <denys.petryniak@gmail.com>
2024-01-14 17:24:17 +0200

    Access state from ProductStore

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <script setup>
 import TheHeader from "@/components/TheHeader.vue";
 import ProductCard from "@/components/ProductCard.vue";
-import products from "@/data/products.json";
 import { useProductStore } from "@/stores/ProductStore";
 
-useProductStore();
+const productStore = useProductStore();
 </script>
 
 <template>
@@ -12,7 +11,7 @@ useProductStore();
     <TheHeader />
     <ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
       <ProductCard
-        v-for="product in products"
+        v-for="product in productStore.products"
         :key="product.name"
         :product="product"
       />
