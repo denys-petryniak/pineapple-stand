e55d64c63d3fcd5338acdaf51afb2c7eeb20b1d9
Denys Petryniak <denys.petryniak@gmail.com>
2024-01-17 13:58:02 +0200

    AuthUserStore: implement dummy visitTwitterProfilet action

--- src/stores/AuthUserStore.js
+++ src/stores/AuthUserStore.js
@@ -3,7 +3,13 @@ import { defineStore } from "pinia";
 export const useAuthUserStore = defineStore("AuthUserStore", {
   state: () => {
     return {
-      username: "Denys Petryniak",
+      username: "denys_petryniak",
     };
+  },
+
+  actions: {
+    visitTwitterProfile() {
+      window.open(`https://twitter.com/${this.username}`, "_blank");
+    },
   },
 });
